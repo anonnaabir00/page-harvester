b639131fbb8991aa2295918fd5c65af092f5c992
Asaduzzaman Abir <anonnaabir00@gmail.com>
2021-07-23 11:36:55 +0600

    Initial Commit

---
+++ page-harvester.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:       Page Harvester
+ * Plugin URI:        https://codember.com
+ * Description:       Fully functional Page Harvester plugin for WordPress. This plugin allows you to create pages automatically based on search query.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Asaduzzaman Abir
+ * Author URI:        https://iamabir.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       plugin-harvester
+ * Domain Path:       /languages
+ */
