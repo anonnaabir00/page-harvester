c6e3add3a9d6868d5fd927b4ebbdd48c98e4d080
Asaduzzaman Abir <anonnaabir00@gmail.com>
2021-08-07 10:43:55 +0600

    Updated

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-jQuery('#submit').click(function(e) {
-    console.log('Start');
-    jQuery.ajax({
-            url:phform_post.ajax_url,
-            type:'POST',
-            dataType:"json",
-            data: {
-                action:'single_post',
-                title:jQuery('#phterm').val(),
-                // content:jQuery('#post_content').val()
-            },
-             success: function(response){
-                    alert('complate');
-            }
+// jQuery('#submit').click(function(e) {
+//     console.log('Start');
+//     jQuery.ajax({
+//             url:phform_post.ajax_url,
+//             type:'POST',
+//             dataType:"json",
+//             data: {
+//                 action:'single_post',
+//                 title:jQuery('#phterm').val(),
+//                 // content:jQuery('#post_content').val()
+//             },
+//              success: function(response){
+//                     alert('Alhamdulillah. Done');
+//             }
            
-    });
+//     });
 
-   return false;
-});
+//    return false;
+// });
