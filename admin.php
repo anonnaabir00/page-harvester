945265a9843abc524d45812639dc3504cf41579d
Asaduzzaman Abir <anonnaabir00@gmail.com>
2021-08-23 12:49:57 +0600

    Added CSF Framework

---
+++ admin.php
@@ -0,0 +1,69 @@
+<?php
+
+    
+
+
+    // Control core classes for avoid errors
+if( class_exists( 'CSF' ) ) {
+
+    //
+    // Set a unique slug-like ID
+    $prefix = 'page_harvester';
+  
+    //
+    // Create options
+    CSF::createOptions( $prefix, array(
+      'menu_title' => 'Page Harvester',
+      'menu_slug'  => 'page-harvester',
+    ) );
+  
+    //
+    // Create a section
+    CSF::createSection( $prefix, array(
+      'title'  => 'Enter Data',
+      'fields' => array(
+  
+        array(
+            'id'      => 'ph-data-list',
+            'type'    => 'textarea',
+            'title'   => 'Data',
+          ),
+
+          array(
+            'id'     => 'ph-post-titles',
+            'type'   => 'repeater',
+            'title'  => 'Text Before Data',
+            'fields' => array(
+          
+              array(
+                'id'    => 'ph-post-titles-text',
+                'type'  => 'text',
+                'title' => 'Before Title'
+              ),
+          
+            ),
+          ),
+
+
+          array(
+            'id'     => 'ph-post-titles_after',
+            'type'   => 'repeater',
+            'title'  => 'Text After Data',
+            'fields' => array(
+          
+              array(
+                'id'    => 'ph-post-titles-text',
+                'type'  => 'text',
+                'title' => 'After Title'
+              ),
+          
+            ),
+          ),
+          
+          
+  
+      )
+    ) );
+  
+  }
+  
