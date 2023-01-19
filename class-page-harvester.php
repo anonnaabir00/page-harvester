8f8cabcb7277df8892d80ec1f3e469fc535943e9
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-01-19 11:53:57 +0600

    Version Number Updated

--- class-page-harvester.php
+++ class-page-harvester.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Page Harvester
  * Plugin URI:        https://codember.com
  * Description:       Fully functional Page Harvester plugin for WordPress. This plugin allows you to create pages automatically based on search query.
- * Version:           2.1
+ * Version:           2.2
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Asaduzzaman Abir
