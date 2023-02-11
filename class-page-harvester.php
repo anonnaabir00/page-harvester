0fbdace5d8f8ed3e5e62250fcc1a151dfa96c52e
Asaduzzaman Abir <anonnaabir00@gmail.com>
2023-02-12 01:17:08 +0600

    Updated

--- class-page-harvester.php
+++ class-page-harvester.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Page Harvester
  * Plugin URI:        https://codember.com
  * Description:       Fully functional Page Harvester plugin for WordPress. This plugin allows you to create pages automatically based on search query.
- * Version:           2.2
+ * Version:           3.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Asaduzzaman Abir
@@ -45,7 +45,40 @@
                   $slug = 'ph_admin';
                   $icon_url =  'dashicons-admin-site-alt2';
                   
-                  add_menu_page(__( $page_title, 'page-harvester' ),$menu_title,$capability,$slug,array( $this,'ph_settings_content'),$icon_url, 99);
+                  add_menu_page(
+                    __( $page_title, 'page-harvester' ),
+                    $menu_title,
+                    $capability,
+                    $slug,
+                    array( $this,'ph_settings_content'),$icon_url, 99);
+
+                    add_submenu_page( 
+                        $slug, __( 'Dumpster Pages', 'page-harvester' ), 
+                        __( 'Dumpster Pages', 'page-harvester' ),
+                        $capability,
+                        $slug,
+                        array( $this,'ph_settings_content' ) );
+
+                    add_submenu_page( 
+                            $slug, __( 'Dumpster GEO', 'page-harvester' ), 
+                            __( 'Dumpster GEO', 'page-harvester' ),
+                            $capability,
+                            'ph_admin#/dumpster-geo',
+                            array( $this,'ph_settings_content' ) );
+
+                  add_submenu_page( 
+                    $slug, __( 'Porta Potty Pages', 'page-harvester' ), 
+                    __( 'Porta Potty Pages', 'page-harvester' ),
+                    $capability,
+                    'ph_admin#/porta-potty',
+                    array( $this,'ph_settings_content' ) );
+
+                    add_submenu_page( 
+                        $slug, __( 'Porta Potty GEO', 'page-harvester' ), 
+                        __( 'Porta Potty GEO', 'page-harvester' ),
+                        $capability,
+                        'ph_admin#/porta-potty-geo',
+                        array( $this,'ph_settings_content' ) );
       
               }
           
@@ -56,10 +89,70 @@
               }
 
             public function ph_insert_dumpster_post(){
-                register_rest_route( 'ph/v1', '/geo/dumpster', array(
+                register_rest_route( 'ph/v1', '/dumpster/geo', array(
                     'methods' => 'POST',
                     'callback' => array( $this, 'ph_insert_dumpster_rest_callback' ),
                 ));
+                
+                register_rest_route( 'ph/v1', '/dumpster/ads', array(
+                    'methods' => 'POST',
+                    'callback' => array( $this, 'ph_insert_dumpster_ads_rest_callback' ),
+                ));
+
+                register_rest_route( 'ph/v1', '/porta-potty/ads', array(
+                    'methods' => 'POST',
+                    'callback' => array( $this, 'ph_insert_portapotty_ads_rest_callback' ),
+                ));
+
+
+                register_rest_route( 'ph/v1', '/porta-potty/geo', array(
+                    'methods' => 'POST',
+                    'callback' => array( $this, 'ph_insert_portapotty_rest_callback' ),
+                ));
+            }
+
+            public function ph_insert_dumpster_ads_rest_callback(WP_REST_Request $request){
+                $value = json_decode($request->get_body());
+                
+                // Create post object
+                $my_post = array(
+                    'post_title'    => wp_strip_all_tags($value->post_title),
+                    'post_content'  => $value->post_content,
+                    'post_type'     => 'page',
+                    'post_status'   => 'publish',
+                    'page_template'  => 'elementor_header_footer',
+                    'post_author'   => 1,
+                );
+                
+                // Insert the post into the database and get id
+                $post_id = wp_insert_post($my_post);
+
+                // Update ACF meta field
+                $city = 'city_state';
+                $phone = 'phone_number';
+                $phone_placeholder = 'phone';
+                $adwords_phone = 'adword_code_phone';
+
+                $adwords_code = '<!-- Google tag (gtag.js) -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10800357225"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag(\'js\', new Date());
+                
+                  gtag(\'config\', \'AW-10800357225\');
+                </script>';
+
+                update_field($city, $value->city, $post_id);
+                update_field($phone, 'tel:'.$value->phone, $post_id);
+                update_field($phone_placeholder, $value->placeholder, $post_id);
+                update_field($adwords_phone, $value->phoneads, $post_id);
+                update_field('adword_code_main', $adwords_code, $post_id);
+
+                // Get Post Permalink
+                $post_url = get_permalink($post_id);
+
+                return $post_url;
             }
     
             public function ph_insert_dumpster_rest_callback(WP_REST_Request $request){
@@ -78,9 +171,84 @@
                 $post_id = wp_insert_post($my_post);
 
                 // Update ACF meta field
-                $meta_selector = 'location_name';
-                $meta_value = $value->location;
-                update_field($meta_selector, $meta_value, $post_id);
+                $location = 'location_name';
+                $city_information = 'city_information';
+
+                update_field($location, $value->location, $post_id);
+                update_field($city_information, $value->information, $post_id);
+
+                // Get Post Permalink
+                $post_url = get_permalink($post_id);
+
+                return $post_url;
+            }
+
+            public function ph_insert_portapotty_ads_rest_callback(WP_REST_Request $request){
+                $value = json_decode($request->get_body());
+                
+                // Create post object
+                $my_post = array(
+                    'post_title'    => wp_strip_all_tags($value->post_title),
+                    'post_content'  => $value->post_content,
+                    'post_type'     => 'page',
+                    'post_status'   => 'publish',
+                    'page_template'  => 'elementor_header_footer',
+                    'post_author'   => 1,
+                );
+                
+                // Insert the post into the database and get id
+                $post_id = wp_insert_post($my_post);
+
+                // Update ACF meta field
+                $city = 'city_state';
+                $phone = 'phone_number';
+                $phone_placeholder = 'phone';
+                $adwords_phone = 'adword_code_phone';
+
+                $adwords_code = '<!-- Google tag (gtag.js) -->
+                <script async src="https://www.googletagmanager.com/gtag/js?id=AW-10800357225"></script>
+                <script>
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag(\'js\', new Date());
+                
+                  gtag(\'config\', \'AW-10800357225\');
+                </script>';
+
+                update_field($city, $value->city, $post_id);
+                update_field($phone, 'tel:'.$value->phone, $post_id);
+                update_field($phone_placeholder, $value->placeholder, $post_id);
+                update_field($adwords_phone, $value->phoneads, $post_id);
+                update_field('adword_code_main', $adwords_code, $post_id);
+
+                // Get Post Permalink
+                $post_url = get_permalink($post_id);
+
+                return $post_url;
+            }
+
+
+            public function ph_insert_portapotty_rest_callback(WP_REST_Request $request){
+                $value = json_decode($request->get_body());
+                
+                // Create post object
+                $my_post = array(
+                    'post_title'    => wp_strip_all_tags($value->post_title),
+                    'post_content'  => $value->post_content,
+                    'post_status'   => 'publish',
+                    'page_template'  => 'elementor_header_footer',
+                    'post_author'   => 1,
+                );
+                
+                // Insert the post into the database and get id
+                $post_id = wp_insert_post($my_post);
+
+                // Update ACF meta field
+                $location = 'location_name';
+                $city_information = 'city_information';
+
+                update_field($location, $value->location, $post_id);
+                update_field($city_information, $value->information, $post_id);
 
                 // Get Post Permalink
                 $post_url = get_permalink($post_id);
--- src/main.js
+++ src/main.js
@@ -3,20 +3,26 @@ import VueRouter from 'vue-router';
 
 
 import App from './Vue/App.vue';
+import Dumpster from './Vue/Pages/Dumpster.vue';
+import PortaPotty from './Vue/Pages/PortaPotty.vue';
+import DumpsterGEO from './Vue/Pages/DumpsterGEO.vue';
+import PortaPottyGEO from './Vue/Pages/PortaPottyGEO.vue';
 
-// Vue.use(VueRouter)
+Vue.use(VueRouter)
 
-// const routes = [
-//     { path: '/', component: Wizard },
-//     { path: '/preloader', component: PreloaderSettings },
-//     { path: '/whitelabel', component: WhiteLabel },
-// ]
+const routes = [
+    { path: '/', component: Dumpster },
+    { path: '/porta-potty', component: PortaPotty },
+    { path: '/dumpster-geo', component: DumpsterGEO },
+    { path: '/porta-potty-geo', component: PortaPottyGEO },
+]
 
-// const router = new VueRouter({routes})
+const router = new VueRouter({routes})
 
 
 new Vue({
     el: '#ph-admin',
     render: h => h(App),
+    router: router,
     // components: { App }
 });
