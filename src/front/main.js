16e1807c6884341c32ab7715d55e2b7eff4602a2
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-01-07 23:00:17 +0600

    Updated

--- src/front/main.js
+++ src/front/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Header from './Header.vue';
 import ConcreteWeightCalculator from './ConcreteWeightCalculator.vue';
 
+
 new Vue({
     el: '#ph-leadform',
     render: h => h(Header),
