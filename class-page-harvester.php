558f212f862cfe8787cc062837436f0caf77706f
Asaduzzaman Abir <anonnaabir00@gmail.com>
2024-02-03 12:03:20 +0600

    Updated Version Number

--- class-page-harvester.php
+++ class-page-harvester.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Page Harvester
  * Plugin URI:        https://primedumpster.com
  * Description:       Fully functional Page Harvester plugin for WordPress. This plugin allows you to create pages automatically based on search query.
- * Version:           8.1
+ * Version:           8.2
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Asaduzzaman Abir
